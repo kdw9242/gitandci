3fc3d32d8da99bf06831ce670090f7e36a7c0d84
Dongwoo Kim <kdw9242@gmail.com>
2016-08-24 13:58:46 +0900

    new function multiply

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -9,4 +9,12 @@ public class Calc
 	public int add(int x, int y) { // add method
 		return x+y;
 	}
+
+	public int subtract(int x, int y) {
+		return x-y;
+	}
+
+	public int mult(int x, int y) {
+		return x*y;
+	}
 }
--- src/test/java/sig/oss/CalcTest.java
+++ src/test/java/sig/oss/CalcTest.java
@@ -18,4 +18,22 @@ public class CalcTest
         Calc c = new Calc();
         assertEquals( "30", c.add(10,20)+"" );
     }
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testSubtract()
+    {
+        Calc c = new Calc();
+        assertEquals( "10", c.subtract(20,10)+"" );
+    }
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testMult()
+    {
+        Calc c = new Calc();
+        assertEquals( "200", c.mult(20,10)+"" );
+    }
 }
